b7b0abb813c580329ecae7cab12568c50bc18c6e
Gyuwon Kim <wonkgo1@gmail.com>
2018-09-18 21:49:05 +0900

    checking api call

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var app = express();
 let port = process.env.PORT || 4000;
 
 app.get('/api/test', (req, res) => {
-  res.send({test : 'testing api server'});
+  let object = {
+    test: 'testing api server3'
+  };
+  res.json(object);
 });
 
 app.listen(port, () => console.log(`API server running at ${port}`));
