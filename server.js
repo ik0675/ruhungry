8eb3e1fa55711c3f3caaa2396ba3f1cabc0c5665
Gyuwon Kim <wonkgo1@gmail.com>
2018-09-25 16:49:18 +0900

    api login / sign up implemented

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let info = require('./mysql_info');
+let mysql = require('mysql');
+let app = express();
+
+let connection = mysql.createConnection(info);
+
+connection.connect((err) => {
+  if (err)
+    throw err;
+  console.log(`Connected to database at localhost`);
+});
 
 let port = process.env.PORT || 4000;
 
-app.get('/api/test', (req, res) => {
-  let object = {
-    test: 'testing api server3'
-  };
-  res.json(object);
+app.use(express.json());
+
+app.post('/api/login', (req, res) => {
+  let id = req.body.id;
+  let password = req.body.password;
+
+  connection.query(`SELECT * FROM account where id='${id}'
+                    AND password='${password}'`,
+                    (err, rows, field) => {
+    if (err)
+      throw err;
+
+    if (rows === []) {
+      // incorrect id or password
+      res.send(400);
+    }
+    else {
+      res.send(200);
+    }
+  });
+})
+
+app.post('/api/sign_up', (req, res) => {
+  let userInfo = req.body;
+  let id = userInfo.id;
+  let password = userInfo.password;
+
+  connection.query(`SELECT * FROM account where id='${id}'`,
+                   (err, rows, field) => {
+    if (err)
+      throw err;
+
+    if (rows !== []) {
+      // user already exists
+      res.send(400);
+    }
+    else {
+      connection.query(`INSERT INTO account(id, password)
+                        VALUES ('${id}', '${password}')`, (err) => {
+        if (err)
+          throw err;
+
+        res.send(200);
+      });
+    }
+  });
 });
 
 app.listen(port, () => console.log(`API server running at ${port}`));
