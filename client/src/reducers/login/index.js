33ef8ed01d0c2ea1006253ebf1c1c3040231c895
Gyuwon Kim <wonkgo1@gmail.com>
2018-12-06 15:54:19 +0900

    redux login initialState defined

--- client/src/reducers/login/index.js
+++ client/src/reducers/login/index.js
@@ -1,7 +1,11 @@
 import * as types from '../../actions/types';
 
 const initialState = {
-
+  id: '',
+  name: '',
+  socket: null,
+  isLogin: 'pending',
+  msg: null,
 }
 
 export default function loginReducers(state = initialState, action) {
