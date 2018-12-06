0743774408b430893da68578bd7222e9e7490357
Gyuwon Kim <wonkgo1@gmail.com>
2018-12-06 15:46:07 +0900

    redux basic structure created and store created at /client/store.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import App from './components/App';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './components/App';
-
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+  <Provider store={ store } >
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
