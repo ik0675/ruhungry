2db824255edbcf81bd4b189a50f3d5653f4b841b
Gyuwon Kim <wonkgo1@gmail.com>
2018-12-07 14:32:16 +0900

    SignUp restructured

--- client/src/components/login/SignUp.js
+++ client/src/components/login/SignUp.js
@@ -1,85 +1,91 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './css/SignUp.css';
 
+const propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+}
+
+const defaultProps = {
+  onSubmit: undefined,
+}
+
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: '',
-      password: '',
-      name: '',
-    }
-
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleIDChange = this.handleIDChange.bind(this);
-    this.handlePWChange = this.handlePWChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  state = {
+    id      : '',
+    password: '',
+    name    : '',
   }
 
-  handleNameChange(e) {
+  handleChange = (e) => {
     this.setState({
-      name: e.target.value,
+      [e.target.name]: e.target.value
     })
   }
 
-  handleIDChange(e) {
-    this.setState({
-      id: e.target.value,
-    })
-  }
-
-  handlePWChange(e) {
-    this.setState({
-      password: e.target.value,
-    })
-  }
-
-  handleSubmit() {
+  handleSubmit = () => {
     this.props.onSubmit(this.state);
     this.setState({
-      id: '',
+      id      : '',
       password: '',
-      name: '',
+      name    : '',
     });
   }
 
   render() {
-      return (
-          <div className="sign">
-              <button className='btn btn-md btn-info btn-block'
-                      type="submit"
-                      onClick={this.props.onToggle}>
-                      회원가입
-              </button>
-                {this.props.info.isToggleOn &&
-                  <div className="form-group">
-                      <input  placeholder="이름"
-                              type="text"
-                              className='form-control'
-                              onChange={this.handleNameChange}
-                              value={this.state.name} />
+    return (
+      <div className="sign">
+        <button
+          className='btn btn-md btn-info btn-block'
+          type="submit"
+          onClick={this.props.onToggle}
+        >
+          회원가입
+        </button>
+        { this.props.info.isToggleOn &&
+          <div className="form-group">
+            <input
+              placeholder="이름"
+              type="text"
+              className='form-control'
+              onChange={this.handleChange}
+              value={this.state.name}
+              name="name"
+            />
 
-                      <input  placeholder="Email"
-                              type="text"
-                              className='form-control'
-                              onChange={this.handleIDChange}
-                              value={this.state.id} />
+            <input
+              placeholder="Email"
+              type="text"
+              className='form-control'
+              onChange={this.handleIDChange}
+              value={this.state.id}
+              name="id"
+            />
 
-                      <input  placeholder="Password"
-                              type="password"
-                              className='form-control'
-                              onChange={this.handlePWChange}
-                              value={this.state.password} />
+            <input
+              placeholder="Password"
+              type="password"
+              className='form-control'
+              onChange={this.handlePWChange}
+              value={this.state.password}
+              name="password"
+            />
 
-                      <button className='btn btn-md btn-secondary btn-block'
-                              onClick={this.handleSubmit} >
-                          제출
-                      </button>
-                  </div>
-              }
+            <button
+              className='btn btn-md btn-secondary btn-block'
+              onClick={this.handleSubmit}
+            >
+              제출
+            </button>
           </div>
-      );
+        }
+      </div>
+    );
   }
 }
+
+SignUp.propTypes = propTypes;
+SignUp.defaultProps = defaultProps;
 
 export default SignUp;
