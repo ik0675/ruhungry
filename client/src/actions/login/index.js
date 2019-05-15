3c01371373a9027388a307c36222d73808b6ce01
Gyuwon Kim <wonkgo1@gmail.com>
2019-05-15 09:12:44 +0900

    io end point changed to / from localhost

--- client/src/actions/login/index.js
+++ client/src/actions/login/index.js
@@ -1,93 +1,92 @@
-import * as types from '../types';
+import * as types from "../types";
 
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 export const dispatchLogin = ({ id, password }) => dispatch => {
-  fetch('/api/login', {
-    method  : 'POST',
-    headers : {
-                'Content-Type': 'application/json'
-              },
-    body    : JSON.stringify({
-                id      : id,
-                password: password,
-              })
+  fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: id,
+      password: password
+    })
   })
-  .then( res => res.json() )
-  .then( data => {
-    if (data.status) {
-      const socket = io('localhost:4000');
-      socket.emit('login', { id: data.user.id, name: data.user.name });
+    .then(res => res.json())
+    .then(data => {
+      if (data.status) {
+        const socket = io("/");
+        socket.emit("login", { id: data.user.id, name: data.user.name });
+        return dispatch({
+          type: types.LOGIN,
+          data: {
+            id: data.user.id,
+            name: data.user.name,
+            userImg: data.user.userImg,
+            socket: socket
+          }
+        });
+      }
+
+      return dispatch({
+        type: types.LOGIN_FAIL,
+        data: {
+          status: data.status,
+          msg: data.msg
+        }
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+export const dispatchSignup = user => dispatch => {
+  fetch("/api/signUp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+    .then(res => res.json())
+    .then(data => {
+      return dispatch({
+        type: types.SIGNUP,
+        data: {
+          status: data.status,
+          msg: data.msg
+        }
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+export const dispatchCheckSession = () => dispatch => {
+  fetch("/api/session")
+    .then(res => res.json())
+    .then(data => {
+      if (!data.status) {
+        return dispatch({ type: types.NO_SESSION });
+      }
+
+      const socket = io("/");
+      socket.emit("login", { id: data.user.id, name: data.user.name });
       return dispatch({
         type: types.LOGIN,
         data: {
-          id      : data.user.id,
-          name    : data.user.name,
-          userImg : data.user.userImg,
-          socket  : socket,
+          id: data.user.id,
+          name: data.user.name,
+          userImg: data.user.userImg,
+          socket: socket
         }
-      })
-    }
-
-    return dispatch({
-      type: types.LOGIN_FAIL,
-      data: {
-              status: data.status,
-              msg   : data.msg,
-            }
+      });
     })
-  })
-  .catch( err => console.log(err) )
-}
+    .catch(err => console.error(err));
+};
 
-export const dispatchSignup = (user) => dispatch => {
-  fetch('/api/signUp', {
-    method  : 'POST',
-    headers : {
-                'Content-Type': 'application/json'
-              },
-    body    : JSON.stringify(user)
-  })
-  .then( res => res.json() )
-  .then( data => {
-    return dispatch({
-      type: types.SIGNUP,
-      data: {
-              status: data.status,
-              msg   : data.msg
-            }
-    })
-  })
-  .catch( err => console.log(err) )
-}
-
-export const dispatchCheckSession = () => dispatch => {
-  fetch('/api/session')
-  .then( res => res.json() )
-  .then( data => {
-    if (!data.status) {
-      return dispatch({ type: types.NO_SESSION })
-    }
-
-    const socket = io('localhost:4000');
-    socket.emit('login', { id: data.user.id, name: data.user.name });
-    return dispatch({
-      type: types.LOGIN,
-      data: {
-        id      : data.user.id,
-        name    : data.user.name,
-        userImg : data.user.userImg,
-        socket  : socket,
-      }
-    })
-  })
-  .catch( err => console.error(err) );
-}
-
-export const dispatchLogout = (socket) => dispatch => {
-  fetch('/api/logout')
-  .then(() => {
-    socket.emit('logout');
+export const dispatchLogout = socket => dispatch => {
+  fetch("/api/logout").then(() => {
+    socket.emit("logout");
     dispatch({ type: types.LOGOUT });
-  })
-}
+  });
+};
