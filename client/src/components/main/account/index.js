d0f4c526f567b61285cd2343898677e858e5f2df
Gyuwon Kim <wonkgo1@gmail.com>
2019-06-05 14:43:21 +0900

    friend status button error in account page fixed

--- client/src/components/main/account/index.js
+++ client/src/components/main/account/index.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import {
   dispatchLoadAccountInfo,
   dispatchFriendRequest,
-  dispatchIsFriends,
-} from '../../../actions/account';
+  dispatchIsFriends
+} from "../../../actions/account";
 
-import TopNav from './TopNav';
-import FriendRequests from './FriendRequests';
-import Posts from '../post/Posts';
-import ChatRooms from '../chat';
+import TopNav from "./TopNav";
+import FriendRequests from "./FriendRequests";
+import Posts from "../post/Posts";
+import ChatRooms from "../chat";
 
-import './css/Account.css';
+import "./css/Account.css";
 
 const propTypes = {
-  myId          : PropTypes.string.isRequired,
-  id            : PropTypes.string.isRequired,
-  name          : PropTypes.string.isRequired,
-  userImg       : PropTypes.string.isRequired,
-  friendStatus  : PropTypes.string.isRequired,
-  load          : PropTypes.func.isRequired,
-  friendRequest : PropTypes.func.isRequired,
-  isFriends     : PropTypes.func.isRequired,
+  myId: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  userImg: PropTypes.string.isRequired,
+  friendStatus: PropTypes.string.isRequired,
+  load: PropTypes.func.isRequired,
+  friendRequest: PropTypes.func.isRequired,
+  isFriends: PropTypes.func.isRequired
 };
 
 class Account extends Component {
   state = {
-    invitation  : 2,
-    id          : this.props.match.params.id,
-    loaded      : false,
+    invitation: 2,
+    id: this.props.match.params.id,
+    loaded: false,
     friendLoaded: false,
-    friendStatus: 'not sent',
+    friendStatus: null
   };
 
   invitationRef = React.createRef();
@@ -45,34 +45,37 @@ class Account extends Component {
     if (!this.state.loaded) {
       this.props.load(id, () => this.setState({ loaded: true }));
     }
-  };
+  }
 
   componentWillReceiveProps(nextProps) {
     if (this.state.id !== nextProps.match.params.id) {
-      this.setState({
-        id    : nextProps.match.params.id,
-        loaded: false,
-      }, () => this.props.load(this.state.id, () => this.setState({ loaded: true })));
+      this.setState(
+        {
+          id: nextProps.match.params.id,
+          loaded: false
+        },
+        () =>
+          this.props.load(this.state.id, () => this.setState({ loaded: true }))
+      );
     }
-  };
+  }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.id !== this.props.myId && !this.state.friendLoaded) {
       const id = this.props.myId;
       const friend_id = this.props.id;
-      this.props.isFriends(
-        id, friend_id,
-        (status) => this.setState({ friendLoaded: true, friendStatus: status })
+      this.props.isFriends(id, friend_id, status =>
+        this.setState({ friendLoaded: true, friendStatus: status })
       );
     }
-  };
+  }
 
-  handleInvitationFilter = (e) => {
+  handleInvitationFilter = e => {
     const val = parseInt(e.target.value, 10);
     if (this.state.invitation !== val) {
       this.setState({ invitation: val });
     }
-  }
+  };
 
   sendFriendRequest = _ => {
     const { id } = this.props;
@@ -90,54 +93,39 @@ class Account extends Component {
           <p>Loading account information...</p>
           <img src="/loading.gif" alt="loading" />
         </div>
-      )
-    } else if (id === '') {
+      );
+    } else if (id === "") {
       return (
         <div className="Account">
           <p>The ID requested doesn't exist...</p>
           <p>This is most likely because the user deactivated the account</p>
         </div>
-      )
+      );
     }
     let button;
     if (myId !== id) {
       if (!friendLoaded) {
         button = <img src="/loading.gif" alt="loading" />;
-      } else if (friendStatus === 'friend') {
+      } else if (friendStatus === "friend") {
+        button = <button className="btn-friend">Friends!</button>;
+      } else if (friendStatus === "not sent") {
         button = (
-          <button className="btn-friend">
-            Friends!
-          </button>
-        );
-      } else if (friendStatus === 'not sent') {
-        button = (
-          <button
-            className="btn-notSent"
-            onClick={this.sendFriendRequest}
-          >
+          <button className="btn-notSent" onClick={this.sendFriendRequest}>
             Send Friend Request
           </button>
         );
-      } else if (friendStatus === 'sent') {
-        button = (
-          <button className="btn-sent">
-            Request Sent
-          </button>
-        );
-      } else if (friendStatus === 'err') {
-        button = (
-          <button className="btn-err">
-            Server Error...
-          </button>
-        )
+      } else if (friendStatus === "sent") {
+        button = <button className="btn-sent">Request Sent</button>;
+      } else if (friendStatus === "err") {
+        button = <button className="btn-err">Server Error...</button>;
       }
     }
 
     const refs = {
-      invitation    : this.invitationRef,
-      friendRequest : this.friendRequestRef,
-      openChatRooms : this.openChatRoomsRef,
-    }
+      invitation: this.invitationRef,
+      friendRequest: this.friendRequestRef,
+      openChatRooms: this.openChatRoomsRef
+    };
     return (
       <div className="Account">
         <TopNav refs={refs} />
@@ -147,7 +135,7 @@ class Account extends Component {
           {button}
         </p>
         <div className="Account-panel">
-          <div className="Account-invitations" ref={this.invitationRef} >
+          <div className="Account-invitations" ref={this.invitationRef}>
             <hr />
             <div className="invitation-filter">
               Invitations
@@ -167,42 +155,47 @@ class Account extends Component {
               accountId={this.props.match.params.id}
             />
           </div>
-          {myId === id &&
-            <div className="Account-friendRequest" ref={this.friendRequestRef} >
+          {myId === id && (
+            <div className="Account-friendRequest" ref={this.friendRequestRef}>
               <hr />
               <p>Friend Requests</p>
               <hr />
               <FriendRequests />
             </div>
-          }
-          {myId === id &&
-            <div className="Account-chat" ref={this.openChatRoomsRef} >
+          )}
+          {myId === id && (
+            <div className="Account-chat" ref={this.openChatRoomsRef}>
               <hr />
               <p>Open Chat Rooms</p>
               <hr />
               <ChatRooms />
             </div>
-          }
+          )}
         </div>
       </div>
     );
-  };
+  }
 }
 
 Account.propTypes = propTypes;
 
 const mapStateToProps = state => ({
-  myId        : state.login.id,
-  id          : state.account.id,
-  name        : state.account.name,
-  userImg     : state.account.userImg,
-  friendStatus: state.account.friendStatus,
+  myId: state.login.id,
+  id: state.account.id,
+  name: state.account.name,
+  userImg: state.account.userImg,
+  friendStatus: state.account.friendStatus
 });
 
 const mapDispatchToProps = {
-  load          : dispatchLoadAccountInfo,
-  friendRequest : dispatchFriendRequest,
-  isFriends     : dispatchIsFriends,
+  load: dispatchLoadAccountInfo,
+  friendRequest: dispatchFriendRequest,
+  isFriends: dispatchIsFriends
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Account));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Account)
+);
